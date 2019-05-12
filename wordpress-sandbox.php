2389303d70d7cc02ee23f334094abe0cb57ce0ff
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-12 19:51:33 +0000

    Plugin scaffold run

---
+++ wordpress-sandbox.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wordpress Sandbox
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wordpress-sandbox
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wordpress_Sandbox
+ */
+
+// Your code starts here.
