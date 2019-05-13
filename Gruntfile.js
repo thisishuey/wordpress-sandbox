87d9673b47e28944333fb88b5c2926cf4ae3dc8c
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-13 01:28:36 +0000

    Run `npx grunt` and remove extraneous tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,14 +24,6 @@ module.exports = function(grunt) {
       }
     },
 
-    wp_readme_to_markdown: {
-      your_target: {
-        files: {
-          "README.md": "readme.txt"
-        }
-      }
-    },
-
     makepot: {
       target: {
         options: {
@@ -51,10 +43,8 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks("grunt-wp-i18n");
-  grunt.loadNpmTasks("grunt-wp-readme-to-markdown");
-  grunt.registerTask("default", ["i18n", "readme"]);
+  grunt.registerTask("default", ["i18n"]);
   grunt.registerTask("i18n", ["addtextdomain", "makepot"]);
-  grunt.registerTask("readme", ["wp_readme_to_markdown"]);
 
   grunt.util.linefeed = "\n";
 };
