e1a0f6c851e96bfcfec6878091c57015106e5f74
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-15 17:59:27 +0000

    Update plugin information

--- wordpress-sandbox.php
+++ wordpress-sandbox.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name:     Wordpress Sandbox
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin Name:     WordPress Sandbox
+ * Plugin URI:      https://github.com/thisishuey/wordpress-sandbox#readem
+ * Description:     Playing around with WordPress and Docker
+ * Author:          Jeff "Huey" Huelsbeck
+ * Author URI:      https://www.carpetelam.com
  * Text Domain:     wordpress-sandbox
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         1.0.0
  *
- * @package         Wordpress_Sandbox
+ * @package         WordPress_Sandbox
  */
 
 // Your code starts here.
